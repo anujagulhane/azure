581930ee436b915d39304208da744639c01781b5
anujagulhane <anujagulhane2000@gmail.com>
2023-02-17 11:28:15 +0530

    added cred

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "azurerm" {
   features {}
   client_id       = "560f2838-1946-4398-a191-3b7a7c484b66"
-  client_secret   = "xfb8Q~GX6ORBXyVnE.n6gcFZj-6Yfs5lUrwoybz-“"
+  client_secret   = "xfb8Q~GX6ORBXyVnE.n6gcFZj-6Yfs5lUrwoybz-"
   tenant_id       = "2c5efb81-d32d-4f77-8537-b68dfbea918f"
   subscription_id = "f6e5dcb5-31c7-4f61-b999-2d4d15ac20f3"
 }
